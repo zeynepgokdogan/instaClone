431138f8bce3c9bca0520231ea77b505d05e23ed
Zeynep <88207798+zeynepgokdogan@users.noreply.github.com>
2025-03-24 14:41:25 +0300

    Initial Commit

---
+++ instaClone/instaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instaCloneApp.swift
+//  instaClone
+//
+//  Created by Zeynep Gökdoğan on 24.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct instaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ instaCloneTests/instaCloneTests.swift
@@ -0,0 +1,16 @@
+//
+//  instaCloneTests.swift
+//  instaCloneTests
+//
+//  Created by Zeynep Gökdoğan on 24.03.2025.
+//
+
+import Testing
+
+struct instaCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
